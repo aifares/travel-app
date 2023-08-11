c9af7b697b1bf79c0eabecf81d8278c6b624d96b
aifares <aifares@buffalo.edu>
2023-08-10 23:09:53 -0400

    add flight data

---
+++ app/utils/getFlightData.ts
@@ -0,0 +1,28 @@
+import { Duffel } from "@duffel/api";
+
+const duffel = new Duffel({
+  token: "duffel_test_PTGHpqlDC118HrhOMGnPDNRU-DVCwLZFuqriClIn1i9",
+});
+
+interface FlightInfoProps {
+  slices: { origin: string; destination: string; departureDate: string }[];
+}
+
+const getFlightInfo = async (slices: FlightInfoProps) => {
+  const offerID = await duffel.offerRequests.create({
+    slices,
+    passengers: [{ type: "adult" }, { type: "adult" }, { age: 1 }],
+    cabin_class: "economy",
+    return_offers: false,
+  });
+
+  const flights = await duffel.offers.list({
+    offer_request_id: offerID.data.id,
+    sort: "total_amount",
+    limit: 1,
+  });
+
+  console.log(flights.data);
+};
+
+export default getFlightInfo;
